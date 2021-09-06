52e0878f81e2999509f0ef13704a56a47efa774d
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-06 14:58:42 +0300

    defined years and month variable

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
 
 var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-var dayOfWeek = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
-var century =19 
-var year = 1994
-var month = 10
-var day = 06
-alert("month")
+var cc,yy,mm,dd
+var century 
+var year
+var month
+var date 
+cc = century
+yy = year
+mm = month
+dd = date
+
