37c6b3038dd2b93048cd03ff4a78d027553c50d0
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-06 19:06:23 +0300

    join dayofweek and male/female variable

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ yy = year
 mm = month
 dd = date
 
+function getDayOfWeek(cc,yy,mm,dd){
+    var dayOfWeek = Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd) % 7 )
+    return dayOfWeek;
+}
+
+/**/
+$(document).ready(function(){
+    $("form#akan").submit(function(event){
+        event.preventDefault()
+        var gender = $('input[name="gender"]:checked').val();
+        var date = $("#date").val()
+        century = parseInt(date[0]+ date[1])
+        year = parseInt(date[2]+ date[3])
+        month = parseInt(date[5]+ date[6])
+        date = parseInt(date[8]+ date[9])
+        if (gender == "male"){
+            var day = getDayOfWeek(century,year,month,date)
+            alert("Your Akan name is " + male[day - 1])
+        } else{
+            var day = getDayOfWeek(century,year,month,date)
+            alert("Your Akan name is " + female[day - 1])
+        }
+    })
+})
