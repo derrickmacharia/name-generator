7efdbd858c8456c51c9d9beb7dbdbefa025db6df
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-06 12:32:22 +0300

    add image and background-color

--- app.js
+++ app.js
@@ -3,5 +3,9 @@
 
 var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
-
+var dayOfWeek = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+var century =19 
+var year = 1994
+var month = 10
+var day = 06
+alert("month")
