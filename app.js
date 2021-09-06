ee38bd4087e8b3fa1ba0b79f96bbec27100cec17
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-06 11:26:02 +0300

    assigned male and females variables

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
+/* Arrays that contains a list of Akan names in respect to the days of the weeks */
+
+var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 
