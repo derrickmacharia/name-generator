23a452c22ff55ffabc2af934d2cf46af5a62f095
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-07 13:57:18 +0300

    make the code to read the gender data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var cc,yy,mm,dd;
+
 var century = 19;
 var year = 94;
 var month = 10;
@@ -12,5 +13,5 @@ cc = century;
 yy = year;
 mm = month;
 dd = date;
-var value = Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
-alert
+var dayOfWeek = Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
+console.log (male[0])
