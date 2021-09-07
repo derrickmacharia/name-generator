3a9d5dfb8a3b6221525c916f84f921a557ba3615
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-07 14:54:14 +0300

    add if statement

--- app.js
+++ app.js
@@ -10,18 +10,26 @@ function dayOfWeek(century,year,month,date){
     yy = year;
     mm = month;
     dd = date;
-    return  Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
+    var value = Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
+    return value
 }
 
 $(document).ready(function(){
-    $("form#akan").submit(function(event){
-        event.preventDefault()
+    $("form#akan").submit(function(e){
+      e.preventDefault() 
         var calendar = $("#date").val()
+        var gender = $('input[name="gender"]:checked').val();
         var dateOfBirth = calendar;
         var century = parseInt(dateOfBirth[0].concat(dateOfBirth[1]));
-        var year = parseInt(dateOfBirth[0].concat(dateOfBirth[1]));
-        var month = parseInt(dateOfBirth[0].concat(dateOfBirth[1]));
-        var date = parseInt(dateOfBirth[0].concat(dateOfBirth[1]));
-        var dayOfWeek = dayOfWeek(century,year,month,date)
+        var year = parseInt(dateOfBirth[2].concat(dateOfBirth[3]));
+        var month = parseInt(dateOfBirth[5].concat(dateOfBirth[6]));
+        var date = parseInt(dateOfBirth[8].concat(dateOfBirth[9]));
+        var day = dayOfWeek(century, year, month, date);
+        if (gender == "male"){
+            alert("Your Akan name is " + male[day - 1])
+        } else{
+            alert("Your Akan name is " + female[day - 1])
+        }
     })
+
 })
