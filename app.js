10a9ad3cc8700a5c524cf516761d8ae3e5a1893f
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-09-07 14:36:54 +0300

    created a function

--- app.js
+++ app.js
@@ -3,15 +3,25 @@
 
 var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var cc,yy,mm,dd;
-var dateOfBirth = "1994-10-14"
-var century = dateOfBirth[0].concat(dateOfBirth[1])
-var year = dateOfBirth[0].concat(dateOfBirth[1])
-var month = dateOfBirth[0].concat(dateOfBirth[1])
-var date = dateOfBirth[0].concat(dateOfBirth[1])
-cc = century;
-yy = year;
-mm = month;
-dd = date;
+
 var dayOfWeek = Math.trunc((((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
 console.log (male[0])
+function dayOfWeek(){
+    var cc,yy,mm,dd;
+    cc = century;
+    yy = year;
+    mm = month;
+    dd = date;
+}
+
+$(document).ready(function(){
+    $("form#akan").submit(function(event){
+        event.preventDefault()
+        var calendar = $("#date").val()
+        var dateOfBirth = calendar;
+        var century = dateOfBirth[0].concat(dateOfBirth[1]);
+        var year = dateOfBirth[0].concat(dateOfBirth[1]);
+        var month = dateOfBirth[0].concat(dateOfBirth[1]);
+        var date = dateOfBirth[0].concat(dateOfBirth[1]);
+    })
+})
